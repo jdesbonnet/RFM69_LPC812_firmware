2e52e9f70449e2f3c8a0a4efde434751c8fe319b
Joe Desbonnet <jdesbonnet@gmail.com>
2015-02-10 13:28:33 +0000

    tidy eeprom emulator

--- src/eeprom.c
+++ src/eeprom.c
@@ -9,74 +9,35 @@
 #include "eeprom.h"
 #include "iap_driver.h"
 
-
-// Store EEPROM page at offset 0x2000 (8kB into flash space)
-//#define CONFIG_FLASH_ADDR_OFFSET     0x2000
-#define CONFIG_FLASH_ADDR_OFFSET     SETTINGS_FLASH_ADDR
-#define CONFIG_FLASH_PAGE            (CONFIG_FLASH_ADDR_OFFSET >> 6)
-#define CONFIG_FLASH_SECTOR_FOR_PAGE (CONFIG_FLASH_PAGE >> 4)
-#define CONFIG_FLASH_PAGE_SIZE       1
-#define CONFIG_FLASH_SECTOR_SIZE     (CONFIG_FLASH_PAGE_SIZE >> 4)
-
-
-// iap_copy_ram_to_flash() must have source data in SRAM. To ensure this is the case
-// copy to buffer we know for sure is in RAM (For example data could be a const
-// string stored in flash: it seems that doesn't work.. flash-to-flash copy
-// is not possible
-static uint8_t eeprom_ram_buffer[64];
-
 // Allocate 64 byte aligned, 64 byte block in flash memory (0x00000000 - 0x00003999) on
 // 16kB LPC812. Assignment to constant ( = {0}) seems necessary to force allocation in
 // flash area.
 const char eeprom_flashpage[64] __attribute__ ((aligned (64))) = {0};
 
-static uint32_t part_id, bootcode_rev, unique_id[4];
-
 /**
  * Write 64 byte page to flash.
+ *
+ * @param data Pointer to 64 byte block of memory to write to flash. This must be in SRAM
+ * area of memory.
+ *
+ * @return 0 for success, negative value for error.
  */
 int32_t eeprom_write (uint8_t *data) {
-
-
 
 	uint32_t iap_status;
 
 	iap_init();
 
-	memcpy (eeprom_ram_buffer, data, 64);
-
 	uint32_t flash_page = (uint32_t)&eeprom_flashpage >> 6;
 	uint32_t flash_sector = flash_page >> 4;
 
-
-#ifdef FALSE
-	/* Q1: What is the part id of this MCU ? */
-	iap_read_part_id(&part_id);
-	if (part_id == 0x00) {
-		return -1;
-	}
-
-	/* Q2: What is the bootROM revision of this MCU ? */
-	iap_read_bootcode_rev(&bootcode_rev);
-	if (bootcode_rev == 0x00) {
-		/* Invalid bootcode rev */
-		return -2;
-	}
-
-	/* Q3: What is the unique ID of this MCU ? Is it same with other MCU ? */
-	iap_read_unique_id(unique_id);
-	if ((unique_id[0] == 0x00) && (unique_id[1] == 0x00)
-			&& (unique_id[2] == 0x00) && (unique_id[3] == 0x00)) {
-		/* Invalid unique id */
-		return -3;
-	}
-#endif
-
+	// Example code checks MCU part ID, bootcode revision number and serial number. There are some
+	// differences in behavior across silicon revisions (in particular to do with ability
+	// to erase multiple sectors at the same time). In this case we require to be able to program
+	// just one sector, so this does not conern us.
 
 
 	/* Prepare the page for erase */
-	//iap_status = (__e_iap_status) iap_prepare_sector(CONFIG_FLASH_SECTOR_FOR_PAGE,
-	//		(CONFIG_FLASH_SECTOR_FOR_PAGE + CONFIG_FLASH_SECTOR_SIZE));
 	iap_status = (__e_iap_status) iap_prepare_sector(flash_sector, flash_sector);
 	if (iap_status != CMD_SUCCESS) return -4;
 
@@ -89,12 +50,17 @@ int32_t eeprom_write (uint8_t *data) {
 	if (iap_status != CMD_SUCCESS) return -6;
 
 	/* Write data to page */
-	//iap_status = (__e_iap_status) iap_copy_ram_to_flash(&eeprom_ram_buffer,
-	//		(void *)CONFIG_FLASH_ADDR_OFFSET, 64);
-	iap_status = (__e_iap_status) iap_copy_ram_to_flash(&eeprom_ram_buffer,
+	iap_status = (__e_iap_status) iap_copy_ram_to_flash(data,
 			&eeprom_flashpage, 64);
 	if (iap_status != CMD_SUCCESS) return -7;
 
 	return 0;
 }
 
+/**
+ * Copy content of EEPROM flash area.
+ */
+int32_t eeprom_read (uint8_t *data) {
+	memcpy (data, eeprom_flashpage, 64);
+	return 0;
+}
--- src/eeprom.h
+++ src/eeprom.h
@@ -1,4 +1,5 @@
 #ifndef EEPROM_H_
 #define EEPROM_H_
 int32_t eeprom_write(uint8_t *data);
+int32_t eeprom_read(uint8_t *data);
 #endif
