6ce04a5b6c81865697c8c72c1f07644c038cbd9f
Joe Desbonnet <jdesbonnet@gmail.com>
2016-12-05 23:40:51 +0000

    inc version to 0.7.1

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H_
 
 //#define VERSION "RFM69 0.6.1"
-#define VERSION "RFM69/9x 0.7.1b"
+#define VERSION "RFM69/9x 0.7.1"
 
 // Experimental directive to load function in RAM to facilitate OTA update
 #define RAM_FUNC __attribute__( ( long_call, section(".data.ramfunc") ) )
